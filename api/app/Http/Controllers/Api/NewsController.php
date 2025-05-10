202b33d4b932258705c45e4aca4e6bd9327067dd
natsuki <natsuki.ss@icloud.com>
2025-05-10 14:13:31 +0900

    ニュース一覧取得の処理追加

--- api/app/Http/Controllers/Api/NewsController.php
+++ api/app/Http/Controllers/Api/NewsController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\News;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
+    /**
+     * ニュース一覧を取得する
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        // id, title, published_atを取得
+        $news = News::select('id', 'title', 'published_at')
+            ->orderBy('published_at', 'asc')
+            ->get();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => [
+                'news' => $news,
+            ],
+        ]);
+    }
 }
